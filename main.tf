061ccd0642736ecb2c8b8cf37bb93ce26f42c5c0
Himanshu <doomedmonk13@gmail.com>
2022-04-22 12:46:12 +0530

    ready to deploy file with nginx running on docker

--- main.tf
+++ main.tf
@@ -1,38 +1,126 @@
 provider "aws" {
     region = "ap-south-1"
-    # access_key = "AKIAV2HOUZPVUROGCWOY"
-    # secret_key = "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"
 }
-resource "aws_vpc" "dev-vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      "Name" = "dev-vpc-1"
-    }
-  
-}
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = "ap-south-1b"
-    tags = {
-      "Name" = "dev-subnet-1"
-    }
-}
-variable "subnet_cidr_block" {
-    description = "Subnet cidr block"
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-}   
-# data "aws_vpc" "dev-vpc" {
-#     default = true
-# }
-# resource "aws_subnet" "dev_subnet_2" {
-#     vpc_id = data.aws_vpc.dev-vpc.id
-#     cidr_block = "172.31.48.0/20"
-#     availability_zone = "ap-south-1b"
-# }
-output "dev-vpc-id" {
-    value = aws_vpc.dev-vpc.id
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+      "Name" = "${var.env_prefix}-vpc"
+    }
 }
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.cidr_block
+
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+      "Name" = "${var.env_prefix}-subnet-1"
+    }
 }
+
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+    }
+    tags = {
+      "Name" = "${var.env_prefix}-route-table"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-internet-gateway" {
+    vpc_id = aws_vpc.myapp-vpc.id
+}
+
+# resource "aws_route_table_association" "myapp-rtb-association" {
+#     subnet_id = aws_subnet.myapp-subnet-1.id
+#     route_table_id = aws_route_table.myapp-route-table.id
+  
+# }
+
+# output "aws_ami" {
+#     value = data.aws_ami.latest-ami.id
+# }
+
+resource "aws_default_route_table" "default-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+    }
+}
+
+resource "aws_security_group" "myapp-sg" {
+    name = "${var.env_prefix}-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+      "Name" = "${var.env_prefix}-sg"
+    }  
+}
+
+data "aws_ami" "latest-ami"{
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+
+resource "aws_instance" "myapp-instance" {
+    ami = data.aws_ami.latest-ami.id
+    instance_type = "t2.micro"
+    availability_zone = "ap-south-1b"
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh_key.key_name
+    tags = {
+      "Name" = "${var.env_prefix}-instance"
+    }
+    user_data = <<EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install docker -y
+                sudo systemctl start docker
+                sudo usermod -aG docker ec2-user
+                docker run -p 8080:80 nginx
+                EOF
+}
+
+resource "aws_key_pair" "ssh_key" {
+    key_name = "server-key"
+    public_key = file("C:\\Users\\doome\\.ssh\\id_rsa.pub")
+}
+
